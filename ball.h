ab8c687ce9bee93e73a3ba0d308da3e73fe7e7d2
nomadbromad <82276073+nomadbromad@users.noreply.github.com>
2021-04-10 15:47:11 +0100

    ball.h file

---
+++ ball.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Ball
+{
+private:
+	Vector2f m_Position;
+	RectangleShape m_Shape;
+
+	float m_Speed = 1500.0f;
+	float m_DirectionX = .2f;
+	float m_DirectionY = .2f;
+
+public:
+	Ball(float startX, float StartY);
+
+	FloatRect getPosition();
+
+	RectangleShape getShape();
+
+	float getXVelocity();
+
+	void reboundSides();
+
+	void reboundBatorTop();
+
+	void reboundBottom();
+
+	void update(Time dt);
+};
